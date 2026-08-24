ed4aba8f9fe5e95572914cb910dd938c0187497e
Flavio Faria <135338397+flavifaria@users.noreply.github.com>
2026-08-24 08:12:49 -0300

    update

---
+++ src/br/com/infotech/dal/ModuloConexao.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.infotech.dal;
+import java.sql.*;
+/**
+ *
+ * @author Flávio
+ */
+public class ModuloConexao {
+    public static Connection connector(){
+        java.sql.Connection conexao = null;
+        //linha chamando o driver
+        String driver = "com.mysql.jdbc.Driver";
+        
+        //armazenando informações do banco 
+        String url="jdbc:mysql//localhost:3306/erp_vendas";
+        String user="root";
+        String password="123456";
+        
+        //estabelecendo a conexão
+        try {
+            //caso de certo 
+            Class.forName(driver);
+            conexao = DriverManager.getConnection(url,user,password);
+            return conexao;        
+        } catch (Exception e) {
+            //caso de errado
+            return null;
+        }
+    }
+}
